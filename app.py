c0c9e5945003917633a709814a76a5e820336de4
Alex Bell <alex.morgan.bell@gmail.com>
2023-10-13 23:19:38 -0400

    persists state better across web restarts

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ file_log_folder = '/home/regolith/Downloads/'
 INSTRUCTIONS_MESSAGE = ' Welcome to CB7 text message voting. Text yes to vote yes, no to vote no, abstain to vote abstain, cause to vote cause. '
 INVALID_INPUT_MESSAGE = 'Your vote was NOT RECORDED, your message was invalid. '
 NOT_VOTING_MESSAGE = 'Not currently open for voting'
+NOT_VALID_NUMBER_MESSAGE = 'We done have a record of your number, tell Alex your name and this number'
 
 def get_vote_from_string(incoming_message):
     if 'cause' in incoming_message or 'Cause' in incoming_message:
@@ -130,8 +131,9 @@ def parse_incoming_text(incoming_number,incoming_msg):
     log_raw_vote_to_file(incoming_number,incoming_msg)
     if not currently_in_a_voting_session:
         return create_response_msg(NOT_VOTING_MESSAGE)
-    voting_member:Voter = members[incoming_number]
-    #TODO check if number is in list, otherwise respond with "You are not in the list, here is your number, this is only available for board members"
+    voting_member:Voter = members[incoming_number] or None
+    if not voting_member:
+        return create_response_msg(NOT_VALID_NUMBER_MESSAGE+' '+incoming_number)
     if check_if_instructions(incoming_msg):
         return create_response_msg(INSTRUCTIONS_MESSAGE)
     vote_cast = get_vote_from_string(incoming_msg)
@@ -139,8 +141,7 @@ def parse_incoming_text(incoming_number,incoming_msg):
         return create_response_msg(INVALID_INPUT_MESSAGE)
     
     vote_log[voting_member.sms_number] = Vote(voting_member,vote_cast)
-    #print(get_summary())
-    #write_to_file()
+
     r = MessagingResponse()
     r.message('Your vote has been recorded, you voted '+vote_cast+' for resolution '+current_vote_name)
     return str(r)
@@ -202,13 +203,17 @@ def stopvoting():
             'message': 'Couldnt stop voting',
         }
         return jsonify(response), 500
+    
+
+@app.route('/isvotingstarted', methods=['GET'])
+def is_voting_started():
+    return jsonify({"isVotingStarted": currently_in_a_voting_session,"currentVoteName":current_vote_name})
 
 
-# TODO, change so that front end polls for is currently in Voting Session, i.e. source of truth is backend. ALSO make it so you can refresh webpage
-# without loosing the state
 # TODO, make everything a nice python class and make the options ENUM
 # TODO, get rid of global variables
-# TODO, deploy somewhere a) to some simple server b) the full aws experience, lambdas, writing to dynamodb, etc etc
+# TODO, god tool that lets you send in a vote as someone specific? Only allowed to some user?
+# TODO, deploy somewhere a) to some simple server b) the full aws experience, lambdas, writing to dynamodb, etc etc ( Lambda Layer? Write to S3 the logs?)
 # TODO, change to REACT to make fluid
-# TODO, Gunicorn for deploying
-# TODO, admin tool? With Login?
+# TODO, Gunicorn for deploying to a server?
+# TODO, admin tool? With Login? to change the list of users and numbers?
